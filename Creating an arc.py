2751d3a437b74eaf049ba870117c41ef7addd964
pyarena <Arman170616>
2021-03-28 10:37:40 +0600

    creating an arc

--- Creating an arc.py
+++ Creating an arc.py
@@ -4,7 +4,7 @@ top = Tk()
 top.geometry('200x300')
 
 c = Canvas(top, bg='yellow', height='300')
-
+arc = c.create_arc((5,10,150,200),start = 0,extent = 150, fill= "pink")
 c.pack()
 
 top.mainloop()
