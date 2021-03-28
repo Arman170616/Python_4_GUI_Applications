11c0fb3aa8b18c6de47d8e7034408c387648f7a7
pyarena <Arman170616>
2021-03-28 16:05:57 +0600

    frame test || a tiny calculator

---
+++ frame.py
@@ -0,0 +1,33 @@
+from tkinter import *
+
+
+
+top = Tk()
+top.geometry('400x400')
+
+frame = Frame(top)
+frame.pack()
+
+leftframe = Frame(top)
+leftframe.pack(side=LEFT)
+
+
+rightframe = Frame(top)
+rightframe.pack(side=RIGHT)
+
+
+btn1 = Button(frame, text='Submit', fg='green', activebackground='green')
+btn1.pack(side=LEFT)
+
+
+btn2 = Button(frame, text='Remove', fg='red', activebackground='red')
+btn2.pack(side=RIGHT)
+
+btn3 = Button(rightframe, text="Add", fg="blue", activebackground = "blue")  
+btn3.pack(side = LEFT)  
+  
+btn4 = Button(leftframe, text="Modify", fg="black", activebackground = "white")  
+btn4.pack(side = RIGHT)  
+
+
+top.mainloop()
