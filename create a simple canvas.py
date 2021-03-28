b44b6165f4da334347574b41eed4edbbd6d654c1
pyarena <Arman170616>
2021-03-28 10:30:37 +0600

    tkinter canvas

---
+++ create a simple canvas.py
@@ -0,0 +1,9 @@
+from tkinter import  *
+
+top = Tk()
+top.geometry('200x200')
+
+c = Canvas(top, bg='yellow', height='200')
+c.pack()
+
+top.mainloop()
---
+++ create mul button.py
@@ -0,0 +1,26 @@
+from tkinter import *
+from tkinter import messagebox
+
+top = Tk()
+
+top.geometry('200x100')
+
+def fun():
+    messagebox.showinfo('hello', 'Red button clicked!')
+    #showinfo(title="About", message="My Window")
+
+def fun1():
+    messagebox.showinfo('danke', 'blue button clicked!')
+
+b1 = Button(top, text="Red", command=fun, activeforeground='red', activebackground='pink', pady=10, padx=15)
+b2 = Button(top, text="Blue",command=fun1,  activeforeground='blue', activebackground='pink', pady=10, padx=15)
+b3 = Button(top, text="Green",  activeforeground='green', activebackground='pink', pady=10, padx=15)
+b4 = Button(top, text="Yellow",  activeforeground='yellow', activebackground='pink', pady=10, padx=15)
+
+
+b1.pack(side=LEFT)
+b2.pack(side=RIGHT)
+b3.pack(side=TOP)
+b4.pack(side=BOTTOM)
+
+top.mainloop()
